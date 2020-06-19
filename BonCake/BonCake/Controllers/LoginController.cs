91ede1d5c949a3c5479ee1fbf71e3a5a35270565
mhys_yuanjing <431971042@qq.com>
2020-06-20 00:04:51 +0800

    登录，注册，母版页修改

--- BonCake/BonCake/Controllers/LoginController.cs
+++ BonCake/BonCake/Controllers/LoginController.cs
@@ -14,28 +14,53 @@ namespace BonCake.Controllers
         {
             return View();
         }
-        public ActionResult LoginUser(LoginUsers user)
+        public ActionResult LoginUser(User user)
         {
             using (BonCakeEntities1 db = new BonCakeEntities1())
             {
-                var loginUser = db.LoginUsers.Where(u => u.loginName.Equals(user.loginName) && u.loginPwd.Equals(user.loginPwd)).SingleOrDefault();
+                var loginUser = db.User.Where(u => u.userName.Equals(user.userName) && u.userPwd.Equals(user.userPwd)).SingleOrDefault();
                 if (loginUser == null)
                 {
                     //提示：用户名或密码错误
+                    loginUser = db.User.Where(u => u.userPhone.Equals(user.userName) && u.userPwd.Equals(user.userPwd)).SingleOrDefault();
+                    if (loginUser == null)
+                    {
+                        //跳回登录方法 
+                        return RedirectToAction("Login");
+                    }
                     
-                    //跳回登录方法 
-                    return RedirectToAction("Login");
                 }
                 //登录成功 跳回首页方法 把用户放入Session
-                //Session["UserName"] = loginUser.userName;
-                //Session["HeadPic"] = loginUser.userPhone;
                 Session["user"] = loginUser;
                 return RedirectToAction("UserCenter");
             }
         }
-        public ActionResult Regist()
+        public ActionResult Regist(User user)
         {
-            return View();
+            using (BonCakeEntities1 db = new BonCakeEntities1())
+            {
+                user.userName = "新注册用户";
+                user.userSex = "男";
+                user.userData = DateTime.Now;
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            
+        }
+        //CheckPhone
+        public JsonResult CheckPhone(string userPhone)
+        {
+            using (BonCakeEntities1 db = new BonCakeEntities1())
+            {
+                var userList = db.User.Where(u => u.userPhone.Equals(userPhone)).ToList();
+                //userList.Count > 0 说明用户名存在
+                //userList.Count <= 0 说明用户名不存在
+                Dictionary<string, object> map = new Dictionary<string, object>();
+                map.Add("valid", userList.Count <= 0);
+                return Json(map);
+            }
+
         }
         public ActionResult UserCenter()
         {
